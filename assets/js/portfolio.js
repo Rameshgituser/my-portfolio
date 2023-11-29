f5d0426bd2d5388e0844f3b2107c3288b580521a
t417 <t417@NTCLT0678>
2023-11-29 21:46:41 +0530

    scroll animation

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -24,10 +24,12 @@ navToggle.addEventListener('click',changemenustate);
          window.addEventListener('scroll',()=>{
 
             let scroll=window.pageYOffset || document.documentElement.scrollTop;
-             console.log(scroll);
+            //  console.log(scroll);
 
              if(scroll>=navheight){
               headerdom.classList.add('.navbar-fixed')
+             
+          
              }
              else{
               headerdom.classList.remove('.navbar-fixed');
@@ -45,6 +47,27 @@ navToggle.addEventListener('click',changemenustate);
     }
 
 
+      window.addEventListener('scroll',reveal);
+
+       function reveal(){
+          var reveals=document.querySelectorAll('.reveal');
+
+          for(var i=0;i<reveals.length;i++){
+              var windowheight=window.innerHeight;
+                var revealtop=reveals[i].getBoundingClientRect().top;
+                var revealpoint=150;
+
+                if(revealtop < windowheight - revealpoint){
+                  reveals[i].classList.add('active');
+                }
+                else{
+                  reveals[i].remove.classList.remove('active');
+                }
+          }
+       }
+
+
     fixednavbar()
     onmenuclick()
+
 
